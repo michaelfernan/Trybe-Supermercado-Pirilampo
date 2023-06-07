c35dbe13def46083bf685b12ddd000a1c65aa196
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-06-07 17:41:29 -0300

    Requisito 5

--- src/5-products-amount.js
+++ src/5-products-amount.js
@@ -2,6 +2,12 @@ const stockProducts = require('./data.json');
 
 const getProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
-};
+  let soma = 0;
 
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    soma += stockProducts[i].quantityInStock;
+  } console.log(soma);
+  return soma;
+};
+getProductsAmount();
 module.exports = { getProductsAmount };
