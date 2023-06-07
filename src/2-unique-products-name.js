a1f97c284e469094d702c8d1db7e4942ae839a19
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-06-07 14:08:31 -0300

    Requisito 2

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -2,6 +2,10 @@ const stockProducts = require('./data.json');
 
 const getUniqueProductsName = () => {
   // Desenvolva seu código dentro dessa função...
+  let nomes = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    nomes.push(stockProducts[i].productName);
+  } return nomes;
 };
 
 module.exports = { getUniqueProductsName };
