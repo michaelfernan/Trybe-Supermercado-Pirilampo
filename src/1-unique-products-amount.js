77a2a3d64b4e6b87f57f9423636e63a098be13be
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-06-07 13:35:23 -0300

    Requisito 1

--- src/1-unique-products-amount.js
+++ src/1-unique-products-amount.js
@@ -2,6 +2,14 @@ const stockProducts = require('./data.json');
 
 const getUniqueProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
+  sum = 0;
+  if (stockProducts.length > 0) {
+    sum = stockProducts.length;
+  } else if (stockProducts === 0) {
+    sum = 0;
+  }
+
+  return sum;
 };
 
 module.exports = { getUniqueProductsAmount };
