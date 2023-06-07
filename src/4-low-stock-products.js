1c74be2192047286d2e94319b128f457822bc415
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-06-07 17:20:47 -0300

    Requisito 4

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -2,6 +2,17 @@ const stockProducts = require('./data.json');
 
 const getLowStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
+
+  let nomes = [];
+
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    if (stockProducts[i].quantityInStock > 0 && stockProducts[i].quantityInStock < 11) {
+      nomes.push(`${stockProducts[i].productName}: 
+      ${+(stockProducts[i].quantityInStock)} unidades`);
+    }
+  } console.log(nomes);
+  return nomes;
 };
+getLowStockProducts();
 
 module.exports = { getLowStockProducts };
