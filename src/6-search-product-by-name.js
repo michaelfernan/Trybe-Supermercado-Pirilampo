d23b781afa302fe43a730f1d995e276750fd949b
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-06-07 19:14:35 -0300

    Requisito 6

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -2,6 +2,19 @@ const stockProducts = require('./data.json');
 
 const searchProductByName = () => {
   // Desenvolva seu código dentro dessa função...
+  let nome = 'Arroz';
+  let result;
+
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    if (nome === (stockProducts[i].productName)) {
+      result = stockProducts[i].description;
+      console.log(result);
+    } else {
+      return null;
+    }
+  } return result;
 };
+
+searchProductByName();
 
 module.exports = { searchProductByName };
