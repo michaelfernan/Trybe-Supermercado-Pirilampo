34363c0dd661ef6b7daff70c07ce1532e93d29d6
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-06-07 14:50:17 -0300

    Requisito 3

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -2,6 +2,12 @@ const stockProducts = require('./data.json');
 
 const getOutOfStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
-};
 
+  let quant = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    if (stockProducts[i].quantityInStock < 1) {
+      quant.push(stockProducts[i].productName);
+    }
+  } console.log(quant); return quant;
+}; getOutOfStockProducts();
 module.exports = { getOutOfStockProducts };
