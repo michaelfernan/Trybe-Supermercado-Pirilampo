1a29bbed3aa007418a4b410ef0df5dbaa8340cb9
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-06-08 18:50:11 -0300

    Requisito 10

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -1,7 +1,28 @@
 const stockProducts = require('./data.json');
 
-const getProductsRichInVitamin = () => {
-  // Desenvolva seu código dentro dessa função...
-};
+function getProductsRichInVitamin() {
+  let result = [];
+
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    if (stockProducts[i].nutritionalInfo && stockProducts[i].nutritionalInfo.vitamins) {
+      let vitamins = stockProducts[i].nutritionalInfo.vitamins;
+      let vit = [];
+      // tive axilio da Aluna michelle Cristina t26 b ( o object pega o valor e a chave separadamente)
+      let value = Object.values(vitamins);
+      let nome = Object.keys(vitamins);
+      let frase = (`${nome} - ${value}`);
+      vit.push(frase);
+
+      result.push({
+        description: stockProducts[i].description,
+        formattedPrice: (`R$ ${stockProducts[i].price}`),
+        vitaminsInformation: vit,
+      });
+    }
+  }
+  console.log(result);
+  return result;
+}
+getProductsRichInVitamin();
 
 module.exports = { getProductsRichInVitamin };
