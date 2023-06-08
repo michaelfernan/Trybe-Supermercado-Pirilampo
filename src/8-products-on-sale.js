0920b5d249380a91e8d656e78922b7de1b2274fc
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-06-08 17:06:42 -0300

    Requisito 8

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -2,6 +2,20 @@ const stockProducts = require('./data.json');
 
 const getProductsOnSale = () => {
   // Desenvolva seu código dentro dessa função...
-};
+
+  let result = [];
+
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    if (stockProducts[i].onSale === true) {
+      let format = stockProducts[i].price;
+      result.push({
+        description: (stockProducts[i].description),
+        formattedPrice: (`R$ ${format}`),
+        onSale: true,
+      });
+    }
+  }
+  return result;
+}; getProductsOnSale();
 
 module.exports = { getProductsOnSale };
