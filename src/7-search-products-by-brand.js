02f8355159ded8a1dec93ac6971b62bdbd9fa57a
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-06-08 17:00:23 -0300

    Requisito 7

--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,7 +1,20 @@
 const stockProducts = require('./data.json');
 
-const searchProductsByBrand = () => {
+const searchProductsByBrand = (param) => {
   // Desenvolva seu código dentro dessa função...
-};
+  let nome = param;
+  let result = [];
 
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    if (nome === (stockProducts[i].brand)) {
+      let format = stockProducts[i].price;
+      result.push({
+        description: (stockProducts[i].description),
+        formattedPrice: (`R$ ${format}`),
+      });
+    }
+  }
+  return result;
+};
+searchProductsByBrand();
 module.exports = { searchProductsByBrand };
