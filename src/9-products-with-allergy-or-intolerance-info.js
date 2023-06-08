adbc91ad112a57a690b2f9314efd61038b96d62d
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-06-08 17:41:36 -0300

    Requisito 9

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -2,6 +2,28 @@ const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
   // Desenvolva seu código dentro dessa função...
+  let result = [];
+
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    if (!stockProducts[i].allergyOrIntolerance) {
+      result.push({
+        description: (stockProducts[i].description),
+        formattedPrice: (`R$ ${stockProducts[i].price}`),
+      });
+
+      // o uso do join foi utizado para a formataçao do array.
+    } else {
+      const alergia = (stockProducts[i].allergyOrIntolerance).join(' ');
+      result.push({
+        description: (stockProducts[i].description),
+        formattedPrice: (`R$ ${stockProducts[i].price}`),
+        allergyOrIntoleranceMessage: (`Pode conter: ${alergia}`),
+
+      });
+    }
+  }
+
+  return result;
 };
 
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
